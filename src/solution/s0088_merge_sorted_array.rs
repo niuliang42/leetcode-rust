f3d6d27cab35630173c17e64d4b87f64396e0b55
牛亮:Liang Niu <niuliang3@gmail.com>
2023-03-22 22:55:41 +0400

    88

--- src/solution/s0088_merge_sorted_array.rs
+++ src/solution/s0088_merge_sorted_array.rs
@@ -30,18 +30,18 @@ pub struct Solution {}
 
 impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        let mut i = m - 1;
-        let mut j = n - 1;
-        let mut z = m + n - 1;
-        while z >= 0 {
-            if i >= 0 && (j < 0 || nums1[i as usize] >= nums2[j as usize]) {
-                nums1[z as usize] = nums1[i as usize];
-                i -= 1;
+        let (mut i1, mut i2) = (m - 1, n - 1);
+
+        for i in (0..m + n).rev() {
+            if i2 < 0 {
+                break;
+            } else if i1 >=0 && nums1[i1 as usize] > nums2[i2 as usize] {
+                nums1[i as usize] = nums1[i1 as usize];
+                i1 -= 1;
             } else {
-                nums1[z as usize] = nums2[j as usize];
-                j -= 1;
+                nums1[i as usize] = nums2[i2 as usize];
+                i2 -= 1;
             }
-            z -= 1;
         }
     }
 }
